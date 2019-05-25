f2b36d329f401b7a5a72919c99040426edbeb2f4
SKupisz <34217509+SKupisz@users.noreply.github.com>
2019-05-25 12:18:14 +0200

    Modified questions

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-var counter = -1;
-var poprawna = "";
-var alfa = "";
+var counter = -1
+var poprawna = ""
+var alfa = ""
 var header = [
   "Od którego roku przyznawana jest złota odznaka?",
   "Ile % obecności na zajęciach w pracowni macierzystej jest wymagane?",
@@ -8,11 +8,12 @@ var header = [
   "W co najmniej ilu imprezach trzeba uczestniczyć?",
   "Honorowy Wychowanek Pałacu Młodzieży nie ma prawa do:",
   "Ile osób max. wchodzi w skład Komisji Odznaki?",
+  "Dyrektorem PM jest:",
   "W jakim dokumencie zapisuje się zdobywców złotej odznaki?",
   "Jak długi staż pałacowy jest wymagany?",
   "Ile jest stopni odznaki?",
-  "Dyrektor,który wprowadził odznakę, to:"
-];
+  "Dyrektor,który stworzył odznakę, to:"
+]
 var answers = [
   ["1960", "1947", "1958", "1961", "1958"],
   ["100%", "80%", "85%", "60%", "80%"],
@@ -26,6 +27,13 @@ var answers = [
     "audiencji u dyrekcji"
   ],
   ["7", "10", "8", "9", "8"],
+  [
+    "Urszula Wacowska",
+    "Beata Szydło",
+    "Ewa Kopacz",
+    "Rafał Trzaskowski",
+    "Urszula Wacowska"
+  ],
   [
     "W złotej księdze odznaki",
     "W dzienniku ustaw RP",
@@ -42,34 +50,34 @@ var answers = [
     "Urszula Wacowska",
     "Jerzy Berek"
   ]
-];
+]
 
 function start() {
-  document.getElementById("start").style.display = "none";
-  counter++;
-  document.getElementById("query").style.display = "block";
-  render(counter);
+  document.getElementById("start").style.display = "none"
+  counter++
+  document.getElementById("query").style.display = "block"
+  render(counter)
 }
 function render(numberOfC) {
   if (numberOfC < header.length) {
-    document.getElementById("query-header").innerHTML = header[numberOfC];
-    alfa = answers[numberOfC];
-    document.getElementById("one").innerHTML = alfa[0];
-    document.getElementById("two").innerHTML = alfa[1];
-    document.getElementById("three").innerHTML = alfa[2];
-    document.getElementById("four").innerHTML = alfa[3];
-    poprawna = alfa[4];
+    document.getElementById("query-header").innerHTML = header[numberOfC]
+    alfa = answers[numberOfC]
+    document.getElementById("one").innerHTML = alfa[0]
+    document.getElementById("two").innerHTML = alfa[1]
+    document.getElementById("three").innerHTML = alfa[2]
+    document.getElementById("four").innerHTML = alfa[3]
+    poprawna = alfa[4]
   } else {
-    document.getElementById("query").style.display = "none";
-    document.getElementById("win").style.display = "block";
+    document.getElementById("query").style.display = "none"
+    document.getElementById("win").style.display = "block"
   }
 }
 function check(num) {
   if (alfa[num - 1] != poprawna) {
-    document.getElementById("query").style.display = "none";
-    document.getElementById("lose").style.display = "block";
+    document.getElementById("query").style.display = "none"
+    document.getElementById("lose").style.display = "block"
   } else {
-    counter++;
-    render(counter);
+    counter++
+    render(counter)
   }
 }
